4a150d46c2ebcf8f9a865b4caae9ec5bf935e4b0
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-04-20 10:47:23 +0700

    Prak 1 - add route post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', function () {
+    return view('welcome');
+});
